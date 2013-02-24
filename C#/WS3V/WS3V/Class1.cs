16594ec42c7c3e41c64e3220c28b5bb869aff9ec
artfulhacker <artfulhacker@echovoice.com>
2013-02-24 01:23:45 -0700

    solution created

---
+++ C#/WS3V/WS3V/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS3V
+{
+    public class Class1
+    {
+    }
+}
