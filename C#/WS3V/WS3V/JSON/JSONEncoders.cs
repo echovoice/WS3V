ba349fd4507479bed7ad3e7ce21cea67441dc695
artfulhacker <artfulhacker@echovoice.com>
2013-02-25 01:47:06 -0700

    added message types and test
    message types added:
    
    gatekeeper
    signature
    howdy
    recover
    send
    receive
    error
    farewell
    terminated

---
+++ C#/WS3V/WS3V/JSON/JSONEncoders.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WS3V.JSON
+{
+    public static class JSONEncoders
+    {
+        public static string EncodeJsStringArray(string[] s)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            if (s != null && s.Length > 0)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i > 0)
+                        sb.Append(',');
+
+                    sb.Append(EncodeJsString(s[i]));
+                }
+            }
+            sb.Append(']');
+
+            return sb.ToString();
+        }
+
+
+        /// Credit http://www.west-wind.com/weblog/posts/2007/Jul/14/Embedding-JavaScript-Strings-from-an-ASPNET-Page
+        /// 
+        /// Edited to add reverse solidus
+        /// 
+        /// <summary>
+        /// Encodes a string to be represented as a string literal. The format
+        /// is essentially a JSON string.
+        /// 
+        /// The string returned includes outer quotes 
+        /// Example Output: "Hello \"Rick\"!\r\nRock on"
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        
+        public static string EncodeJsString(string s)
+        {
+            //return HttpUtility.JavaScriptStringEncode(s, true);
+
+            StringBuilder sb = new StringBuilder(s.Length);
+            sb.Append("\"");
+            foreach (char c in s)
+            {
+                switch (c)
+                {
+                    case '\"':
+                        sb.Append("\\\"");
+                        break;
+                    case '\\':
+                        sb.Append("\\\\");
+                        break;
+                    case '/':
+                        sb.Append("\\/");
+                        break;
+                    case '\b':
+                        sb.Append("\\b");
+                        break;
+                    case '\f':
+                        sb.Append("\\f");
+                        break;
+                    case '\n':
+                        sb.Append("\\n");
+                        break;
+                    case '\r':
+                        sb.Append("\\r");
+                        break;
+                    case '\t':
+                        sb.Append("\\t");
+                        break;
+
+                    case '<':
+                        sb.Append("\\u003C");
+                        break;
+
+                    case '>':
+                        sb.Append("\\u003E");
+                        break;
+
+                    default:
+                        int i = (int)c;
+                        if (i < 32 || i > 127)
+                        {
+                            sb.AppendFormat("\\u{0:X04}", i);
+                        }
+                        else
+                        {
+                            sb.Append(c);
+                        }
+                        break;
+                }
+            }
+            sb.Append("\"");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#/WS3V/WS3V/MessageTypes/terminated.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WS3V.JSON;
+
+namespace WS3V.MessageTypes
+{
+    /// <summary>
+    /// Built from WS3V Specifications v1
+    /// http://ws3v.org/spec.json#terminated
+    /// </summary>
+    
+    public class terminated
+    {
+        private const int id = 26;
+
+        public int error_code { get; set; }
+        public string error_message { get; set; }
+        public string error_url { get; set; }
+
+        public terminated()
+        {
+            error_code = 0;
+            error_message = null;
+            error_url = null;
+        }
+
+        public terminated(int error_code)
+        {
+            this.error_code = error_code;
+            error_message = null;
+            error_url = null;
+        }
+
+        public terminated(int error_code, string error_message)
+        {
+            this.error_code = error_code;
+            this.error_message = error_message;
+            error_url = null;
+        }
+
+        public terminated(int error_code, string error_message, string error_url)
+        {
+            this.error_code = error_code;
+            this.error_message = error_message;
+            this.error_url = error_url;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            sb.Append(id);
+            sb.Append(',');
+            sb.Append(error_code);
+
+            if (error_message != null)
+            {
+                sb.Append(',');
+                sb.Append(JSONEncoders.EncodeJsString(error_message));
+
+                if (error_url != null)
+                {
+                    sb.Append(',');
+                    sb.Append(JSONEncoders.EncodeJsString(error_url));
+                }
+            }
+
+            sb.Append(']');
+
+            return sb.ToString();
+        }
+    }
+}
